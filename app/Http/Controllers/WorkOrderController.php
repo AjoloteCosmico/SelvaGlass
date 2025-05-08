f0c1501677bdd36602fbbd2c78dff9cf71cb75b4
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2025-05-08 13:47:13 -0600

    asf

--- app/Http/Controllers/WorkOrderController.php
+++ app/Http/Controllers/WorkOrderController.php
@@ -10,7 +10,9 @@ use App\Models\Customer;
 use App\Models\Item;
 use App\Models\WorkOrder;
 use DB;
-
+use PDF;
+use Milon\Barcode\Facades\DNS1DFacade as DNS1D;
+use Milon\Barcode\Facades\DNS2DFacade as DNS2D;
 use Illuminate\Support\Facades\Hash;
 class WorkOrderController extends Controller
 {
@@ -49,7 +51,6 @@ class WorkOrderController extends Controller
     }
 
     public function store_partidas(Request $request){
-
         //si hay otros campos guardar
         return redirect()->route('work_orders.index');
     }
@@ -57,9 +58,35 @@ class WorkOrderController extends Controller
     public function show($id){
         $WorkOrder=WorkOrder::find($id);
         $Items=Item::where('work_order_id',$id)->get();
-        $Ticket=substr(Hash::make($WorkOrder->id.$WorkOrder->type),0,10);
+        $Ticket=substr(Hash::make($WorkOrder->id.$WorkOrder->type),20,30);
         $Customer=Customer::find($WorkOrder->customer_id);
         return view('work_orders.show',compact('WorkOrder','Items','Customer','Ticket'));
+    }
+    public function ticket($id){
+        $WorkOrder=WorkOrder::find($id);
+        $Items=Item::where('work_order_id',$id)->get();
+    
+        // Combinar ID y tipo para formar el texto del código de barras
+        $barcodeText = $WorkOrder->type . '-' . $WorkOrder->id; // Ejemplo: "PED-ABC123"
+        // Generar código de barras CODE 128 (formato común para letras y números)
+        $barcodeHTML = DNS1D::getBarcodeHTML($barcodeText, 'C128');
+        // Opcional: Generar como SVG o PNG
+        $barcodeSVG = DNS1D::getBarcodeSVG($barcodeText, 'C128');
+        $barcodePNG = DNS1D::getBarcodePNG($barcodeText, 'C128');
+        // Crear PDF a partir de una vista Blade
+        $pdf = Pdf::loadView('orders.barcode_pdf', [
+            'barcodeHTML' => $barcodeHTML,
+            'barcodeText' => $barcodeText
+        ]);
+
+        // Opciones del PDF (opcional)
+        $pdf->setPaper('A4', 'portrait'); // Tamaño y orientación
+
+        // Descargar el PDF directamente
+        return $pdf->download('codigo_barras_' . $orderId . '.pdf');
+
+        
+        
     }
     public function destroy($id){
         
